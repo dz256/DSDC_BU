d0321f601dafd822320dfdd57e4dbbe5511141f6
dana zemel <dzemel@bu.edu>
2019-11-25 17:50:44 -0500

    hide credentials, reformat output, code adjustment

--- pubmed_search.py
+++ pubmed_search.py
@@ -4,70 +4,73 @@
 Created on Thu Nov 14 11:37:23 2019
 
 @author: sambrown
+@edited by dz256 
 """
+from configure import * #crediential of app - if you don't have that file tool won't work 
+from warnings import warn
+import datetime
+import numpy as np
 
 import requests
 import xmltodict
 from operator import itemgetter
 from collections import OrderedDict
 
-def search(query, numResponses = 100, retreived = ("authors", "pub_date", "journal", "open_src", "pdf_links", "doi", "id", "title", "abstract"), constraints = {"authors" : "", "start_date" : "", "end_date" : "", "within_days" : 0, "affiliations" : "", "journals" : ""}, returnAsList = False):
+def search(query, numResponses = 100, retreived = 'all', constraints = None, pushToDB = False):
     # PubMed search function
-    # Exclusive inputs:
-    #       1. Query (str): A search query
-    #       2. Query (int): A single ID to retreive information for
-    #       3. Query (list(int)): A list of ID's (up to 500) to retreive information for
-    # Other inputs, valid with all:
-    #       1. Type of data (authors, publication date, affiliation, journal, ...)
-    #       2. Behavior with outputs (return as lists, or push to database)
-    # Other inputs, valid with one:
-    #       1. Number of responses (default = 100)
-    #           Valid only with a search query (str)
-    #       1. Constraints (authors, publication date, affiliation, journal, ...)
-    #           Valid only with a search query (str)
-    # Always returned outputs:s
-    #       1. Request status
-    #
-    # Optional outputs
-    #       2. List of requested outputs (includes boolean for "if opensource", as well as link to pdf if true)
-
+    # Inputs:
+    #       Query (str/int/list(int)): A search query, artical ID, or list of artical IDs
+    #       numResponses (int): maximum number of responses returned - OPTIONAL
+    #                           ONLY VALID IF SEARCH QUERY PROVIDED
+    #       retreived (str/list(str)): data type to be returned. Options are: 
+    #                 ['all','authors', 'pub_date', 'journal', 'open_src',  
+    #                   'pdf_links','doi', 'id', 'title', 'abstract']
+    #       constraints (dict): constraints for search query. Valid fields:
+    #                 ['authors', 'start_date', 'end_date', 'within_days', 
+    #                   'affiliations', 'journals']. 
+    #       pushToDB (bool): for dev purposes only - push results to *OUR* db
+    # Outputs:
+    #       Request status (int): status request (200 == successful)
+    #       Data (dict): a dictionary with the data requested, or error details 
+    #                    if request was unsuccessful
+   
     # First, set up some parameters that will be needed for the function
-    searchTool = "esearch"
-    summaryTool = "esummary"
-    fetchTool = "efetch"
-    postTool = "epost"
-    pmcDB = "pmc"
-    pubMedDB = "pubmed"
-    maxNumIDs = 500
-    maxIDsWithoutPost = 200 # The threshold for using the post method to upload an id list
-    programToolName = "DSDC" # To inform the requst server of our name
-    programEmail = "samuelgb@bu.edu" # To inform the request server how to contact us if there is a problem (please don't screw something up with my name on it)
-    baseURL = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/{tool}.fcgi?db={db}&rettype=xml" + f"&tool={programToolName}&email={programEmail}"
+    infoTypes = ["authors", "pub_date", "journal", "open_src", "pdf_links", "doi", "title", "abstract"]
     
-
+#    locals().update(credentials) # credentials == dict loaded from configure.py 
+    baseURL = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/{tool}.fcgi?db={db}&rettype=xml" + f"&tool={credentials['programToolName']}&email={credentials['programEmail']}"
+    
+    # error handling on request info type:
+    if retreived == 'all':
+        # retrieve all data
+        retreived = infoTypes
+    elif isinstance(retreived,str):
+        if retreived not in infoTypes:
+            raise ValueError(retreived +" is not a supported info type. Options are: %s" % infoTypes)
+        retreived = [retreived]
+    elif isinstance(retreived,list):
+        retreived = list(set(retreived) & set(infoTypes))
+        if len(retreived) == 0:
+            raise ValueError("request dose not contain any supported info type. Options are: %s" % infoTypes)
+               
     # Next, parse the inputs
     requestURL = ""
     if isinstance(query, str):
         # If our query is a string, then it is a search query.  Start building the request
         
-        # Error check the input
+        # User input validation
         if numResponses > 100000:
-            raise Exception("Cannot currently request more than 100,000 responses at a time.  It CAN be done, just...bug Sam or Dana about it")
-
-        dateConstraintStr = ""
-        if constraints["within_days"] != 0:
-            # If the within_days parameter is set...
-            if (constraints["start_date"] != "" or constraints["end_date"] != ""):
-                raise Exception("Can only have one date restriction: either within_days or a start and/or end date")
-
-            # Generation a constraint string based on this input
-            dateConstraintStr = f"&reldate={constraints['within_days']}"
-        elif constraints["start_date"] != "" and constraints["end_date"] != "":
-            # If the start/end date parameter is being used (they must be used together)
-            dateConstraintStr = f"&mindate={constraints['start_date']}&maxdate={constraints['end_date']}"
-        
+            warn("""We decided to set a limit of max 100,000 responses at a time. \n  
+                 if you REALLY want more than the first 100,000 bug Sam or Dana about it. """)
+            numResponses = 100000
+        if isinstance(constraints,dict):
+            # parse constraints string and add to URL
+            dateConstraintStr = createConstraintStr(constraints)
+        else:
+            dateConstraintStr = ""
+                
         # Send the query, along with the constraint information
-        requestURL = baseURL.format(db=pubMedDB, tool=searchTool) + f"&term={query}" + dateConstraintStr + f"&retmax={numResponses}"
+        requestURL = baseURL.format(db=credentials['pubMedDB'], tool=credentials['searchTool']) + f"&term={query}" + dateConstraintStr + f"&retmax={numResponses}"
         response = requests.get(requestURL)
         
         # TODO: Error check the types of responses (expecting a 200)
@@ -79,32 +82,28 @@ def search(query, numResponses = 100, retreived = ("authors", "pub_date", "journ
         # If we are only requesting a single id, then turn it into a list of size 1
         idList = [query]
     elif isinstance(query, list):
-        # If we received a list, then just assign it directly to the idList variable
-        
-        # TODO: Perform some error-checking to ensure that each value in the list is an int
-        
-        if len(query) > maxNumIDs:
-            raise Exception("Can only request up to " + str(maxNumIDs) + " IDs at once")
-            
-        idList = query;
+        if not isinstance(query[0],int):
+            raise TypeError("""Query can be a search str, id (int), or list of 
+                            ids (list of integers). The list provided contains
+                            %s """ % type(query[0]))
+        idList = query
+        if len(idList) > maxNumIDs:
+            warn("Can only request up to " + str(maxNumIDs) + " IDs at once")
+            idList = idList[0:maxNumIDs]
     else:
-        raise Exception("Query must be a str (for a search term), an int (for a single document ID), or a list of ints (for a number of document IDs)")
+        raise TypeError("Query must be a str (for a search term), an int (for a single document ID), or a list of ints (for a number of document IDs)")
     
     # By this point, we should have a list of id's (idList) of all of the items that we want information on
-    
-    # First, check if we're actually getting anything, before we go to all the trouble
-    if len(retreived) == 0:
-        # If there isn't any data that the user wants, then just return the idList
-        return idList
-    
-    # If we actually need to communicate with the server, then form the request
+       
+    # form the request:
     # First, create a string list of the ID's
     idStr = "&id=" + ",".join(map(str, idList))
     
     # Depending on how many ID's are provided, it's kind of nice to use the POST method to upload them
+    maxIDsWithoutPost = credentials['maxIDsWithoutPost']
     if len(idList) > maxIDsWithoutPost:
         # Upload the ID's using post
-        uploadRequestURL = baseURL.format(db=pubMedDB, tool=postTool) + idStr
+        uploadRequestURL = baseURL.format(db=credentials['pubMedDB'], tool=credentials['postTool']) + idStr
         postResponse = requests.get(uploadRequestURL)
         
         # Get the query key and webenv from the response
@@ -119,7 +118,7 @@ def search(query, numResponses = 100, retreived = ("authors", "pub_date", "journ
         fetchRequestIdStr = idStr
     
     # Now, perform the fetch
-    fetchRequestURL = baseURL.format(db=pubMedDB, tool=fetchTool) + fetchRequestIdStr
+    fetchRequestURL = baseURL.format(db=credentials['pubMedDB'], tool=credentials['fetchTool']) + fetchRequestIdStr
     fetchResponse = requests.get(fetchRequestURL)
     fetchResponseDict = xmltodict.parse(fetchResponse.text)
     
@@ -129,56 +128,47 @@ def search(query, numResponses = 100, retreived = ("authors", "pub_date", "journ
     allArticleDataLists = list(map(itemgetter('Article'), allCitationInfoLists))    
     
     # Now that we have the results of the fetch (after a long time, probably), pull out all of the interesting information
-    resultsDict = {"ids" : idList}
+    resultsDict = { i : {} for i in idList }
     
-    # Get authors
-    if 'authors' in retreived:
-        allAuthors = []
-        
+    infoMaps = {'authors':"allArticleDataLists[ind]['AuthorList']['Author']",
+                'pub_date':"allArticleDataLists[ind]['ArticleDate']",
+                'journal': "allArticleDataLists[ind]['Journal']",
+                'title':"allArticleDataLists[ind]['ArticleTitle']",
+                'doi':"[val['#text'] for val in allData[ind]['PubmedData']['ArticleIdList']['ArticleId'] if val['@IdType'] == 'doi']",        
+                "open_src":"3", 
+                "pdf_links":"3",
+                "abstract":"3"
+                }
+    
+
         # If we need the authors, then extract all valid authors
-        allAuthorListMetas = list(map(itemgetter('AuthorList'), allArticleDataLists))
-        allAuthorLists = list(map(itemgetter('Author'), allAuthorListMetas))
-        
-#        allAuthors = [[authorList] if isinstance(authorList, OrderedDict) else authorList for authorList in allAuthorLists]
-        
-        for authorList in allAuthorLists:
-            if isinstance(authorList, OrderedDict):
-                 # If this author list is an OrderedDict, then it's actually just an author
-#                 try:
-#                     authorList['Affiliations'] = list(authorList['AffiliationInfo'].values())
-#                     authorList.pop('AffiliationInfo')
-#                 except:
-#                     authorList['Affiliations'] = []
-                 allAuthors.extend([authorList])
-            else:
-                 # If this is a list of OrderedLists, then go through each one and get the required data 
-#                 for author in authorList:
-#                     try:
-#                         author['Affiliations'] = list(author['AffiliationInfo'].values())
-#                         author.pop('AffiliationInfo')
-#                     except:
-#                         author['Affiliations'] = []
-                 allAuthors.extend(authorList)
-        
-        resultsDict['authors'] = allAuthors
+    for ind,ids in enumerate(idList):
+        for rType in retreived:
+            resultsDict[ids][rType] = eval(infoMaps[rType])
+ 
+               
+#        for authorList in allAuthorLists:
+#            if isinstance(authorList, OrderedDict):
+#                 # If this author list is an OrderedDict, then it's actually just an author
+##                 try:
+##                     authorList['Affiliations'] = list(authorList['AffiliationInfo'].values())
+##                     authorList.pop('AffiliationInfo')
+##                 except:
+##                     authorList['Affiliations'] = []
+#                 allAuthors.extend([authorList])
+#            else:
+#                 # If this is a list of OrderedLists, then go through each one and get the required data 
+##                 for author in authorList:
+##                     try:
+##                         author['Affiliations'] = list(author['AffiliationInfo'].values())
+##                         author.pop('AffiliationInfo')
+##                     except:
+##                         author['Affiliations'] = []
+#                 allAuthors.extend(authorList)
+#        
+#        resultsDict['authors'] = allAuthors
         
     # Get publication date
-    if 'pub_date' in retreived:
-        resultsDict['pub_date'] = list(map(itemgetter('ArticleDate'), allArticleDataLists))
-        
-    # Get Journal information
-    if 'journal' in retreived:
-        resultsDict['journal'] = list(map(itemgetter('Journal'), allArticleDataLists))
-        
-    # Get Title
-    if 'title' in retreived:
-        resultsDict['title'] = list(map(itemgetter('ArticleTitle'), allArticleDataLists))
-        
-    # Get DOI
-    if 'doi' in retreived:
-        allPubmedMetadata = list(map(itemgetter('PubmedData'), allData))
-        allArticleIdLists = list(map(itemgetter('ArticleIdList'), allPubmedMetadata))
-        resultsDict['doi'] = [[idDict['#text'] for idDict in articleIDList['ArticleId'] if idDict['@IdType'] == 'doi'] for articleIDList in allArticleIdLists]
                    
 #    # Get Abstract
         # TODO: Deal with some articles not having abstracts!!!
@@ -190,4 +180,46 @@ def search(query, numResponses = 100, retreived = ("authors", "pub_date", "journ
     
     # A test for now, to see how well the function works
     return resultsDict, fetchResponseDict, fetchRequestURL
+
+
+def createConstraintStr(constraints):
+    
+    assert isinstance(constraints,dict), "constraints must be a dict. constraint type: %r" % type(constraints)
+
+    conTypes = ['authors','start_date','end_date','within_days','affiliations',
+                'journals']
+    cons = list(constraints.keys())
+    
+    if len(set(cons) & set(conTypes)) == 0:
+        warn("cons dose not contain any supported constraint types. Options are: %s" % conTypes)
+        return ""
+        
+    invalidCons = np.setdiff1d(cons,conTypes).tolist()
+    if len(invalidCons)>0:
+        warn(""" %s are not valid constraint fields and were ignored. 
+             valid constraints are: %s""" % (invalidCons,conTypes))
+    
+    cons = list(set(cons) & set(conTypes))
+    if "within_days" in cons:
+        if "start_date" in cons or "end_date" in cons:
+            warn("""Can only have one date restriction: either within_days 
+                     or a start and/or end date. ONLY the 'within_days' 
+                     constraint was used for this search """)
+    
+        # Generation a constraint string based on this input
+        dateConstraintStr = f"&reldate={constraints['within_days']}"
+    # TODO: make sure dates are in acceptable format: [YYYY/MM/DD, YYYY/MM, YYYY]
+    elif "start_date" in cons:
+        if "end_date" in cons:
+            dateConstraintStr = f"&mindate={constraints['start_date']}&maxdate={constraints['end_date']}"
+        else: 
+            d = datetime.datetime.today()
+            constraints["end_date"] = d.strftime("%Y/%m/%d")
+            dateConstraintStr = f"&mindate={constraints['start_date']}&maxdate={constraints['end_date']}"
+    elif "end_date" in cons:
+        constraints["start_date"] = "1900"
+        dateConstraintStr = f"&mindate={constraints['start_date']}&maxdate={constraints['end_date']}"
+    
+    # TODO: hundle all other constraint types
+    return dateConstraintStr
     
